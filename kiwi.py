112e5d6fbdbbefeee1ffb0d85a57828c9e67b6db
Marketa Novackova <m.kopecna@centrum.cz>
2017-07-22 15:18:09 +0200

    add complete data for booking

--- kiwi.py
+++ kiwi.py
@@ -1,6 +1,6 @@
 import requests
 import click
-from datetime import datetime
+from datetime import datetime, date, time
 from pprint import pprint
 from bs4 import BeautifulSoup
 
@@ -33,38 +33,50 @@ def url(from_, date, to):
                 print(city['id'], city['name'])
                 to_id = city['id']
 
-    #url = 'https://jizdenky.regiojet.cz/Booking/from/10202002/to/10202003/tarif/REGULAR/departure/20170722/retdep/20170722/return/false?0'
-    url = 'https://jizdenky.regiojet.cz/Booking/from/{}/to/{}/tarif/REGULAR/departure/{}/retdep/{}/return/false?0'.format(from_id, to_id, date_parsed, date_parsed)
+    url = 'https://jizdenky.regiojet.cz/Booking/from/10202002/to/10202003/tarif/REGULAR/departure/20170722/retdep/20170722/return/false?0'
+    #url = 'https://jizdenky.regiojet.cz/Booking/from/{}/to/{}/tarif/REGULAR/departure/{}/retdep/{}/return/false?0'.format(from_id, to_id, date_parsed, date_parsed)
     r = session.get(url)
-    #url = 'https://jizdenky.regiojet.cz/Booking/from/10202002/to/10202003/tarif/REGULAR/departure/20170722/retdep/20170722/return/false?0-1.IBehaviorListener.0-mainPanel-routesPanel&_=0'
-    url = 'https://jizdenky.regiojet.cz/Booking/from/{}/to/{}/tarif/REGULAR/departure/{}/retdep/{}/return/false?0-1.IBehaviorListener.0-mainPanel-routesPanel&_=0'.format(from_id, to_id, date_parsed, date_parsed)
+    url = 'https://jizdenky.regiojet.cz/Booking/from/10202002/to/10202003/tarif/REGULAR/departure/20170722/retdep/20170722/return/false?0-1.IBehaviorListener.0-mainPanel-routesPanel&_=0'
+    #url = 'https://jizdenky.regiojet.cz/Booking/from/{}/to/{}/tarif/REGULAR/departure/{}/retdep/{}/return/false?0-1.IBehaviorListener.0-mainPanel-routesPanel&_=0'.format(from_id, to_id, date_parsed, date_parsed)
     r = session.get(url)
     print(r.status_code)
     print(r.headers['content-type'])
     print(url)
     #print(r.headers['content-type'])
 
-    print(r.text)
+    #print(r.text)
+    date_departure = datetime.strptime(date, '%Y-%m-%d').date()
 
     soup = BeautifulSoup(r.text, 'html.parser')
-    time_departure = soup.find_all('div', class_="col_depart gray_gradient", limit=1)
-    print(time_departure)
-    import pdb; pdb.set_trace()
+
+    time_departure_soup = soup.find('div', class_="col_depart gray_gradient").text
+    print(time_departure_soup)
+    time_departure = datetime.strptime(time_departure_soup, '%H:%M').time()
+    departure = datetime.combine(date_departure, time_departure)
+
+    time_arrival_soup = soup.find('div', class_="col_arival gray_gradient").text
+    print(time_arrival_soup)
+    time_arrival = datetime.strptime(time_departure_soup, '%H:%M').time()
+    arrival = datetime.combine(date_departure, time_arrival)
+
+    free_seats = soup.find('div', class_="col_space gray_gradient").text
+
+    price = soup.find('div', class_="col_price").text
 
 
-'''
-[{
-  "departure": "2017-12-20 12:45:00",
-  "arrival": "2017-12-20 15:00:00",
-  "from": "Praha",
-  "to": "Ostrava",
-  "free_seats": 40,
-  "price": 100.0,
-  "type": "train", # optional (train/bus)
-  "from_id": 47374743, # optional (student agency id)
-  "to_id": 26383230 # optional (student agency id)
-}]
-'''
+    #import pdb; pdb.set_trace()
+    stdout = [{
+        "departure": departure,
+        "arrival": arrival,
+        "from": from_,
+        "to": to,
+        "free_seats": int(free_seats),
+        "price": int(price[:4]),
+        #"type": "train", # optional (train/bus)
+        "from_id": from_id, # optional (student agency id)
+        "to_id": to_id # optional (student agency id)
+    }]
+    pprint(stdout)
 
 
 if __name__ == '__main__':
